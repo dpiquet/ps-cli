12aef35a1aebc47c59fd56a9d70c7f905fabc30e
Damien PIQUET <piqudam@gmail.com>
2014-12-18 11:05:39 +0100

    check if config/settings.inc.php exists before loading prestashop core to avoid silent errors

--- ps-cli.php
+++ ps-cli.php
@@ -24,25 +24,52 @@
 #	
 #
 
+/* Define some $_SERVERS var to avoid errors */
+$_SERVER['REQUEST_URI'] = '';
 
-/** Load Prestashop Core **/
+
+/*=================================
+|
+|	Load Prestashop Core
+|
+\==================================**/
+
 if (!defined('_PS_ADMIN_DIR_'))
         define('_PS_ADMIN_DIR_', getcwd());
 
 if (!defined('PS_ADMIN_DIR'))
         define('PS_ADMIN_DIR', _PS_ADMIN_DIR_);
 
+/*
+   Prestashop checks if config/settings.inc.php exists
+   before loading. If it does not exists, it performs
+   header('location'). ps-cli must check for this before
+   loading prestashop core
+*/
+
+if (! file_exists(_PS_ADMIN_DIR_.'/../config/settings.inc.php') ) {
+	echo "Prestashop seems not installed ! (no config/settings.inc.php found)\n";
+	die();
+}
+
 require(_PS_ADMIN_DIR_.'/../config/config.inc.php');
 require(_PS_ADMIN_DIR_.'/functions.php');
 
-/** Load ps-cli functions */
+
+/*====================================
+|
+|	 Load ps-cli functions
+|
+\=====================================*/
+
 require_once('ps-cli_modules.php');
 require_once('ps-cli_core.php');
 
-//print_module_list('all');
+
+print_module_list('all');
 //enable_module('statscheckup');
 //disable_module('statscheckup');
 //core_list_changed_files(); 
-core_check_version();
+//core_check_version();
 
 ?>
