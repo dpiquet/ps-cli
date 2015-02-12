6e60670d955b66df1f0487471ecd9e880002e537
Damien PIQUET <piqudam@gmail.com>
2015-02-12 22:33:06 +0100

    Phar generator cleanup

--- make_phar.php
+++ make_phar.php
@@ -6,54 +6,10 @@ $phar = new Phar($buildRoot . "/ps-cli.phar",
 	FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
 	"ps-cli.phar");
 
-
-
-# include all files in the phar archive
-function load_dir_files($path) {
-	$dir = opendir($path);
-
-	global $srcRoot;
-
-	while(false !== ($cur = readdir($dir))) {
-
-		$key = str_replace( './' . $srcRoot . '/', '', $path . '/' . $cur);
-
-		if($cur == '.' or $cur == '..') {
-			continue;
-		}
-
-		if(is_dir($path . '/' . $cur)) {
-			echo "goin in $path/$cur directory\n";
-			load_dir_files($path.'/'.$cur);
-		}
-		else {
-			echo "Adding $path/$cur as $key to phar archive\n";
-			$phar[$key] = file_get_contents($path . '/' . $cur);
-		}
-	}
-
-	return;
-}
-
-$phar->startBuffering();
-
-//we must find where we are before loading files
-//load_dir_files('./' . $srcRoot);
-//
 $phar->buildFromDirectory(dirname(__FILE__) . '/src/');
 
-//$phar->setStub( $phar->createDefaultStub("boot_phar.php") );
+$phar->setStub( $phar->createDefaultStub("boot_phar.php") );
 
-$phar->setStub( <<<EOF
-#!/usr/bin/env php
-<?php
-Phar::mapPhar();
-include 'phar://ps-cli.phar/boot_phar.php';
-__HALT_COMPILER();
-?>
-EOF
-);
-
-$phar->stopBuffering();
+echo "Generated $buildRoot/ps-cli.phar\n";
 
 ?>
