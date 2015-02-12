7725e0e2fa52f10adfde8670a2b0080ba0d8a7f7
Damien PIQUET <piqudam@gmail.com>
2015-02-12 23:20:20 +0100

    Manual stub creation to add php shebang

--- make_phar.php
+++ make_phar.php
@@ -2,13 +2,21 @@
 $srcRoot = "src";
 $buildRoot = "build";
   
-$phar = new Phar($buildRoot . "/ps-cli.phar", 
-	FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
-	"ps-cli.phar");
+$phar = new Phar($buildRoot . '/ps-cli.phar', 0, 'ps-cli.phar');
 
 $phar->buildFromDirectory(dirname(__FILE__) . '/src/');
 
-$phar->setStub( $phar->createDefaultStub("boot_phar.php") );
+//$phar->setStub( $phar->createDefaultStub("boot_phar.php") );
+
+$phar->setStub(<<<EOS
+#!/usr/bin/env php
+<?php
+Phar::mapPhar();
+include 'phar://ps-cli.phar/boot_phar.php';
+__HALT_COMPILER();
+?>
+EOS
+);
 
 echo "Generated $buildRoot/ps-cli.phar\n";
 
